820fe6ca5084713c6457fd3eb98353c1aca75a0b
Underscores <84211119+itsunderscores@users.noreply.github.com>
2021-12-01 13:38:25 -0600

    Add files via upload

---
+++ settings.h
@@ -0,0 +1,6 @@
+// Sniper Settings
+int timeout = 3000; //Timeout per snipe request in milliseconds
+int delayperrequest = 50; //Delay per snipe request in milliseconds 
+int requests = 8; //How many requests will be made per account
+string proxy;
+string sniped;
